703fb42735165172e8f8ee8852284a83b5fd548d
victorbg202 <72407530+victorbg202@users.noreply.github.com>
2020-10-26 17:12:05 +0100

    Primer commit
    Estat inicial de la web

--- MiSegundaWeb/src/app/app-routing.module.ts
+++ MiSegundaWeb/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IniciComponent } from './inici/inici.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", pathMatch: "full", redirectTo: "inici"},
+  {path: "inici", component: IniciComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- MiSegundaWeb/src/app/app.module.ts
+++ MiSegundaWeb/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IniciComponent } from './inici/inici.component';
 import { QuiSomComponent } from './qui-som/qui-som.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IniciComponent,
-    QuiSomComponent
+    QuiSomComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
