85b4b02479f2c10430d289a9256308ad94c9c647
victorbg202 <victorbg202@gmail.com>
2020-10-30 19:25:41 +0100

    Formularios modificaciones
    Pendiente agregar inputs para pasar la informacion

--- MiSegundaWeb/src/app/events/events.component.ts
+++ MiSegundaWeb/src/app/events/events.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { CartaEventsComponent } from './carta-events/carta-events.component';
+import { GrupEvents } from 'src/models/events.model';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.css']
 })
+
 export class EventsComponent implements OnInit {
 
   opcionEventos: string = 'all';
+  mostrarInfo: string = "";
+  erDNI = '^[0-9]{8}[A-Za-z]{1}$';
+
+
 
   constructor() { }
 
@@ -18,4 +25,8 @@ export class EventsComponent implements OnInit {
     this.opcionEventos = opcionEventos;
   }
 
+  enviarDatos(nombre: string, lloc: string, email: string, text: string){
+    console.log(nombre, lloc);
+    CartaEventsComponent.cartas.push(new GrupEvents(nombre,lloc ,'assets/ufo.svg', text,email));
+  }
 }
